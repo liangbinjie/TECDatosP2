f98883a719af72e65d432a4ab0c2695af7168daa
niparra21 <130106793+niparra21@users.noreply.github.com>
2023-10-04 13:34:40 -0600

    Update nodoProducto.h

--- nodoProducto.h
+++ nodoProducto.h
@@ -1,8 +1,28 @@
 #ifndef NODOPRODUCTO_H
 #define NODOPRODUCTO_H
 
+#include <iostream>
+using namespace std;
+
 class NodoProducto {
-	
+	public:
+		int id;
+		int idPais;
+        int idCiudad;
+		int idRest;
+        int idMenu;
+        int FB;
+		string nombre;
+		NodoProducto* izq;
+		NodoProducto* der;
+		friend class arbolProducto;
+
+		NodoProducto();
+		NodoProducto(int id, int idPais, int idCiudad, int idRest, int idMenu, string nombre);
+		
 };
+
+typedef NodoProducto* pnodoProducto;
+
 
 #endif
